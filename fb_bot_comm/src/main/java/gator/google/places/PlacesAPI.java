2f3ea7bb32353182c4ffe2d8315389dc9d3a4fce
BATTULA MANOJ <BATTULA MANOJ>
2017-01-21 20:47:11 -0500

    added filerlocation2

--- fb_bot_comm/src/main/java/gator/google/places/PlacesAPI.java
+++ fb_bot_comm/src/main/java/gator/google/places/PlacesAPI.java
@@ -1,5 +1,8 @@
 package gator.google.places;
 
+import java.util.*;
+
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -13,6 +16,7 @@ import org.apache.http.util.EntityUtils;
 import com.google.gson.Gson;
 
 import gator.google.contract.NearbyResponse;
+import gator.google.contract.Result;
 
 public class PlacesAPI {
 
@@ -21,6 +25,15 @@ public class PlacesAPI {
 
 	private static final HttpClient client = HttpClientBuilder.create().build();
 	private static final HttpGet httpGet = new HttpGet(NEARBY_URL + API_KEY);
+	private static final String[] places = {"amusement_park", "aquarium", "art_gallery",
+											"bar", "beauty_salon", "bowling_alley", "cafe",
+											"casino", "church", "clothing_store", "convenience_store",
+											"furniture_store", "hindu_temple", "library", "movie_theater",
+											"museum", "night_club", "park", "restaurant", "shopping_mall",
+											"spa", "stadium", "university", "zoo"
+											};
+			
+			
 
 	public static String getNearbyPlaces(double latitude, double longitude) throws Exception {
 		URIBuilder builder = new URIBuilder(NEARBY_URL);
@@ -30,14 +43,18 @@ public class PlacesAPI {
 		httpGet.setURI(builder.build());
 		HttpResponse response = client.execute(httpGet);
 
-		System.out.println(httpGet.getURI());
-
-		// System.out.println(EntityUtils.toString(response.getEntity(),
-		// "UTF-8"));
-		NearbyResponse nearbyResponse = new Gson().fromJson(EntityUtils.toString(response.getEntity(), "UTF-8"),
-				NearbyResponse.class);
+		String body = EntityUtils.toString(response.getEntity(), "UTF-8");
+		System.out.println(body);
+		NearbyResponse nearbyResponse = new Gson().fromJson(body, NearbyResponse.class);
 		System.out.println(nearbyResponse.getResults().size());
 		return null;
 	}
 
+	public static void locationsFilter1(NearbyResponse nearbyResponse){
+		List<Result> locations = nearbyResponse.getResults();
+		for(Result location:locations){
+			String place_id = location.getPlace_id();
+			System.out.println(place_id);
+		}
+	}
 }
